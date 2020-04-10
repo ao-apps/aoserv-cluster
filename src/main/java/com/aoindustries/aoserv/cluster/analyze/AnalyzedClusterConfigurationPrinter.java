69078420354140f83d3944e6df02e1014e59d430
AO Industries, Inc. <support@aoindustries.com>
2020-04-09 19:25:01 -0500

    Converted top comment to LGPLv3 style.

--- src/main/java/com/aoindustries/aoserv/cluster/analyze/AnalyzedClusterConfigurationPrinter.java
+++ src/main/java/com/aoindustries/aoserv/cluster/analyze/AnalyzedClusterConfigurationPrinter.java
@@ -1,7 +1,24 @@
 /*
- * Copyright 2008-2011, 2020 by AO Industries, Inc.,
- * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
- * All rights reserved.
+ * aoserv-cluster - Cluster optimizer for the AOServ Platform.
+ * Copyright (C) 2008-2011, 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aoserv-cluster.
+ *
+ * aoserv-cluster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aoserv-cluster is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aoserv-cluster.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.aoserv.cluster.analyze;
 
