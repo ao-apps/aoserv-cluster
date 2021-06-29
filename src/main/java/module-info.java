721a21fe35aacc32980ed12fc087bf26129da971
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:49:22 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * aoserv-cluster - Cluster optimizer for the AOServ Platform.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aoserv-cluster.
+ *
+ * aoserv-cluster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aoserv-cluster is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aoserv-cluster.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoindustries.aoserv.cluster {
+	exports com.aoindustries.aoserv.cluster;
+	exports com.aoindustries.aoserv.cluster.analyze;
+	exports com.aoindustries.aoserv.cluster.optimize;
+	// Direct
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	// Java SE
+	requires java.logging;
+}
