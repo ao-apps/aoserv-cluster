4b06c15c63fbf29bd03c2fc1a296fa4cea1c3fda
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:49:25 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/aoindustries/aoserv/cluster/analyze/AnalyzedClusterConfigurationPrinter.java
+++ src/main/java/com/aoindustries/aoserv/cluster/analyze/AnalyzedClusterConfigurationPrinter.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-cluster - Cluster optimizer for the AOServ Platform.
- * Copyright (C) 2008-2011, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2008-2011, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -33,7 +33,7 @@ import java.util.List;
  *
  * @author  AO Industries, Inc.
  */
-public abstract class AnalyzedClusterConfigurationPrinter {
+public final class AnalyzedClusterConfigurationPrinter {
 
 	/** Make no instances. */
 	private AnalyzedClusterConfigurationPrinter() {throw new AssertionError();}
